26c4cc7581f7f1b61a9eb2ed5240ca9896e8924e
Will Schmidt <will.schmidt@gmail.com>
2019-10-08 07:59:22 +1300

    First commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const http = new easyHTTP()
+
+// Get Posts
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     console.log(posts)
+//   }
+// })
+
+// Get Single Post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post) {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     console.log(post)
+//   }
+// })
+
+// Create Data
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post'
+}
+// Create Post
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(
+  err,
+  post
+) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(post)
+  }
+})
