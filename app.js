6f692520398fef51c87c8d8191a8c8418c4b2b70
Will Schmidt <will.schmidt@gmail.com>
2019-10-09 07:30:54 +1300

    Add additional API calls

--- app.js
+++ app.js
@@ -24,7 +24,19 @@ const data = {
   body: 'This is a custom post'
 }
 // Create Post
-http.post('https://jsonplaceholder.typicode.com/posts', data, function(
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(
+//   err,
+//   post
+// ) {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     console.log(post)
+//   }
+// })
+
+// Update post
+http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(
   err,
   post
 ) {
@@ -32,5 +44,17 @@ http.post('https://jsonplaceholder.typicode.com/posts', data, function(
     console.log(err)
   } else {
     console.log(post)
+  }
+})
+
+// Delete post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(
+  err,
+  response
+) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(response)
   }
 })
